5bee03d6b560c503c3c0e1b6c693203c8747e311
AkashChaudhare <74315730+AkashChaudhare@users.noreply.github.com>
2025-04-02 17:56:31 +0530

    Create 1193.sql

---
+++ 1193.sql
@@ -0,0 +1,11 @@
+select 
+    substr(trans_date, 1,7) as month,
+    country,
+    count(1) as trans_count,
+    sum(case when state='approved' then 1 else 0 end) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from
+    transactions
+group by
+    month, country
