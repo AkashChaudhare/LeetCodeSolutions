5eaced8711a62725e6dff07111222647da692b47
AkashChaudhare <74315730+AkashChaudhare@users.noreply.github.com>
2025-12-17 14:00:54 +0530

    Create 347.py

---
+++ 347.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = {}
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+
+        heap = []
+        for num, freq in counter.items():
+            heapq.heappush(heap, (freq, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for freq, num in heap]
